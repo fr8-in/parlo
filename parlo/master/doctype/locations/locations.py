a0e728aa2da03161f9425249160036bb8b6adbb7
prasanthmurugasamy <prasanth.m@fr8.in>
2023-08-23 13:47:39 +0530

    New Commit

--- parlo/master/doctype/locations/locations.py
+++
@@ -1,8 +0,0 @@
-# Copyright (c) 2023, Digitify.app and contributors
-# For license information, please see license.txt
-
-# import frappe
-from frappe.model.document import Document
-
-class Locations(Document):
-	pass
