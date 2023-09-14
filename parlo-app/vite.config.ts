4e4b07e97c12dcc8fd3408a496f16f16ad114666
prasanthmurugasamy <prasanth.m@fr8.in>
2023-09-14 18:17:30 +0530

    routes fixed

--- parlo-app/vite.config.ts
+++ parlo-app/vite.config.ts
@@ -1,27 +1,28 @@
 import path from 'path';
 import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react'
+import react from '@vitejs/plugin-react';
 import proxyOptions from './proxyOptions';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [react()],
-	server: {
-		port: 8080,
-		proxy: proxyOptions
-	},
-	build: {
-		outDir: '../parlo/public/parlo-app',
-		emptyOutDir: true,
-		target: "esnext",
-		  rollupOptions: {
-			treeshake: true, 
-			onwarn(warning ,warn ) {
-			  if (warning.code === 'MODULE_LEVEL_DIRECTIVE') {
-				return
-			  }
-			  warn(warning)
-			}
-		}
-	},
+  plugins: [react()],
+  server: {
+    port: 8080,
+    proxy: proxyOptions,
+  },
+  base:'/parlo',
+  build: {
+    outDir: '../parlo/public/parlo',
+    emptyOutDir: true,
+    target: 'esnext',
+    rollupOptions: {
+      treeshake: true,
+      onwarn(warning, warn) {
+        if (warning.code === 'MODULE_LEVEL_DIRECTIVE') {
+          return;
+        }
+        warn(warning);
+      },
+    }
+  },
 });
